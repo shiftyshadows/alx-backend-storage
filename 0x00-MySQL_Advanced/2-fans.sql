f4898a7322924f55917ff5b8fbd60a9f63a4337d
ShiftyShadows <purple.quasar@proton.me>
2024-11-14 14:56:50 +0300

    2. Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,1 +1,6 @@
-
+-- Select the country of origin and total fans, grouped by origin
+-- Then order the results by the total number of fans in descending order
+SELECT origin, SUM(nb_fans) AS total_fans
+FROM bands
+GROUP BY origin
+ORDER BY total_fans DESC;
