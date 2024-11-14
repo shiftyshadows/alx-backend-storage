04b95cd0d4cc1bd1723f10da229ddaaf3d1d2127
ShiftyShadows <purple.quasar@proton.me>
2024-11-14 16:12:40 +0300

    7. Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,14 +5,16 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    DECLARE avg_score FLOAT;
+    DECLARE avg_score FLOAT DEFAULT 0;
 
-    -- Calculate the average score for the given user_id
-    SET avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+    -- Calculate the average score for the given user_id, handling NULL for single or no entries
+    SELECT IFNULL(AVG(score), 0) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
 
     -- Update the average_score field in the users table
     UPDATE users
-    SET average_score = IFNULL(avg_score, 0)
+    SET average_score = avg_score
     WHERE id = user_id;
 END //
 
