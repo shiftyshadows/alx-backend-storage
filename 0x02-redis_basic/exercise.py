860443e4d504c558506f2e1ba7dc2b3d16e5c281
ShiftyShadows <purple.quasar@proton.me>
2025-01-07 00:47:00 +0300

    2. Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,12 +4,14 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
     def __init__(self):
-        """Initialize the Cache class."""
+        """
+           Initialize the Cache class.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
@@ -26,3 +28,48 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, None]:
+        """
+           Retrieve data from Redis and optionally convert it.
+
+           Args:
+            key (str): The key to retrieve.
+            fn (Optional[Callable]): A function to convert the data.
+
+           Returns:
+            Union[str, bytes, int, None]: The retrieved data,
+            possibly converted.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        return fn(data) if fn else data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+           Retrieve a string from Redis.
+
+           Args:
+            key (str): The key to retrieve.
+
+           Returns:
+            Optional[str]: The retrieved string or None
+            if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+           Retrieve an integer from Redis.
+
+           Args:
+            key (str): The key to retrieve.
+
+           Returns:
+            Optional[int]: The retrieved integer or None
+            if the key does not exist.
+        """
+        return self.get(key, fn=int)
