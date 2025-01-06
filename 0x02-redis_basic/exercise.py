2bd5cff0f81b997da4d68203306bc4f67eb28174
ShiftyShadows <purple.quasar@proton.me>
2025-01-07 00:51:35 +0300

    2. Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,24 +5,34 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count calls to a method using Redis."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = f"{method.__qualname__}"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
     def __init__(self):
-        """
-           Initialize the Cache class.
-        """
+        """Initialize the Cache class."""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-           Store data in Redis with a randomly generated key.
+        Store data in Redis with a randomly generated key.
 
-           Args:
+        Args:
             data (Union[str, bytes, int, float]): The data to store.
 
-           Returns:
+        Returns:
             str: The generated key.
         """
         key = str(uuid.uuid4())
@@ -33,13 +43,13 @@ class Cache:
         self, key: str, fn: Optional[Callable] = None
     ) -> Union[str, bytes, int, None]:
         """
-           Retrieve data from Redis and optionally convert it.
+        Retrieve data from Redis and optionally convert it.
 
-           Args:
+        Args:
             key (str): The key to retrieve.
             fn (Optional[Callable]): A function to convert the data.
 
-           Returns:
+        Returns:
             Union[str, bytes, int, None]: The retrieved data,
             possibly converted.
         """
@@ -50,12 +60,12 @@ class Cache:
 
     def get_str(self, key: str) -> Optional[str]:
         """
-           Retrieve a string from Redis.
+        Retrieve a string from Redis.
 
-           Args:
+        Args:
             key (str): The key to retrieve.
 
-           Returns:
+        Returns:
             Optional[str]: The retrieved string or None
             if the key does not exist.
         """
@@ -63,13 +73,13 @@ class Cache:
 
     def get_int(self, key: str) -> Optional[int]:
         """
-           Retrieve an integer from Redis.
+        Retrieve an integer from Redis.
 
-           Args:
+        Args:
             key (str): The key to retrieve.
 
-           Returns:
-            Optional[int]: The retrieved integer or None
-            if the key does not exist.
+        Returns:
+            Optional[int]: The retrieved integer or None if
+            the key does not exist.
         """
         return self.get(key, fn=int)
